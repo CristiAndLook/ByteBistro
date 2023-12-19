4a7eb8934734bf3838b68ce022b1a45034d1d142
CristiAndLook <krhistian1902@gmail.com>
2023-12-19 16:17:08 -0500

    Basic template with spring

---
+++ src/main/java/com/example/ByteBistro/ByteBistroApplication.java
@@ -0,0 +1,13 @@
+package com.example.ByteBistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteBistroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ByteBistro/ByteBistroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ByteBistro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
